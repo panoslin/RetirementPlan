a2265137af057f46d68ef818b0e4fd1778bb44d6
Panos <panoslin@foxmail.com>
2021-07-10 02:28:37 +0800

    init proj

---
+++ build_data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by panos on 2021/7/10
+# IDE: PyCharm
+
+
+def build_data(
+
+):
+    pass
+
+if __name__ == '__main__':
+    build_data(
+
+    )
