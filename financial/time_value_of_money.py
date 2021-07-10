e940fad291bb3f268cb494b30beef251ccf72fe5
Panos <panoslin@foxmail.com>
2021-07-11 02:41:52 +0800

    add: function pmt_with_down_pmt

--- financial/time_value_of_money.py
+++ financial/time_value_of_money.py
@@ -4,11 +4,23 @@
 # IDE: PyCharm
 """
 Reference:
+
 https://github.com/microsoft/referencesource/blob/master/Microsoft.VisualBasic/runtime/msvbalib/Financial.vb
+
 """
 
+import datetime
 
-def pv(__rate, __nper, __pmt=0, __fv=0, __type=0):
+nper = lambda x: x - datetime.datetime.now().year
+
+
+def pv(
+        __rate: float,
+        __nper: int,
+        __pmt: float = 0,
+        __fv: float = 0,
+        __type: bool = 0
+):
     """
     Calculate the present value
 
@@ -29,7 +41,13 @@ def pv(__rate, __nper, __pmt=0, __fv=0, __type=0):
                ) / (1 + __rate) ** __nper
 
 
-def fv(__rate, __nper, __pmt=0, __pv=0, __type=0):
+def fv(
+        __rate: float,
+        __nper: int,
+        __pmt: float = 0,
+        __pv: float = 0,
+        __type: bool = 0
+) -> float:
     """
     Calculate the future value
 
@@ -51,7 +69,13 @@ def fv(__rate, __nper, __pmt=0, __pv=0, __type=0):
                )
 
 
-def pmt(__rate, __nper, __pv=0, __fv=0, __type=0):
+def pmt(
+        __rate: float,
+        __nper: int,
+        __pv: float = 0,
+        __fv: float = 0,
+        __type: bool = 0
+):
     """
     Calculate the regular payment
 
@@ -68,6 +92,37 @@ def pmt(__rate, __nper, __pv=0, __fv=0, __type=0):
         return (
                        (-__fv - __pv * (1 + __rate) ** __nper) * __rate
                ) / ((1 + __rate * __type) * ((1 + __rate) ** __nper - 1))
+
+
+def pmt_with_down_pmt(
+        year,
+        start_year,
+        end_year,
+        loan_rate,
+        down_pmt_percentage,
+        amount,
+
+):
+    """
+    return the month payment along with the down payment if any
+    :param year:
+    :param start_year:
+    :param end_year:
+    :param loan_rate:
+    :param down_pmt_percentage:
+    :param amount:
+    :return:
+    """
+    if start_year <= year < end_year:
+        regular_pmt = pmt(
+            loan_rate / 12,
+            (end_year - start_year) * 12,
+            (1 - down_pmt_percentage) * amount
+        )
+        down_pmt = -amount * down_pmt_percentage / 12 if year == start_year else 0
+        return regular_pmt + down_pmt
+    else:
+        return 0
 
 
 if __name__ == '__main__':
