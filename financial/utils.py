d961069185d2d2ca14adcbb936e3eaa5bfcd519c
Panos <panoslin@foxmail.com>
2021-07-17 02:50:07 +0800

    add: df2excel in utils

--- financial/utils.py
+++ financial/utils.py
@@ -4,6 +4,8 @@
 # IDE: PyCharm
 import datetime
 import time
+import pandas as pd
+from pathlib import Path
 
 str2datetime = lambda x: datetime.datetime.fromtimestamp(
     time.mktime(
@@ -13,6 +15,38 @@ str2datetime = lambda x: datetime.datetime.fromtimestamp(
         )
     )
 )
+
+
+def df2excel(
+        df,
+        file_name=Path(__file__).parent.joinpath(
+            f'retirement-{datetime.datetime.today().strftime("%Y-%m-%d")}.xlsx'
+        ).absolute(),
+        num_format_column='F:R'
+
+):
+    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    writer = pd.ExcelWriter(
+        file_name,
+        engine='xlsxwriter'
+    )
+    df.to_excel(
+        writer,
+        index=False,
+    )
+    # set columns format
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '#,##0'})
+    worksheet.set_column(num_format_column, None, format1)
+    # set column width
+    for column in df:
+        column_length = max(df[column].astype(str).map(len).max(), len(column))
+        col_idx = df.columns.get_loc(column)
+        writer.sheets['Sheet1'].set_column(col_idx, col_idx, column_length + 1)
+    # close and save file
+    writer.save()
+
 
 if __name__ == '__main__':
     pass
