684aff1c137c72cb9be3bbb83485333e4f7484c7
Panos <panoslin@foxmail.com>
2023-04-26 21:54:11 +0800

    modify: wechat verification path

--- marilyn/survey/urls.py
+++ marilyn/survey/urls.py
@@ -8,6 +8,6 @@ from . import views
 
 urlpatterns = [
     path('survey/', views.index, name='index'),
-    path('392e5faa99df48edbda687cc7571a7b8.txt/', views.wechat_verification, name='wechat_verification'),
+    path('392e5faa99df48edbda687cc7571a7b8.txt', views.wechat_verification, name='wechat_verification'),
     # path('report', views.report, name='report'),
 ]
