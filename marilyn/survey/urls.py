2f4e7e83963c1225964466b5d3a9f01c67f0e792
Panos <panoslin@foxmail.com>
2023-04-28 01:24:34 +0800

    modify: url paths

--- marilyn/survey/urls.py
+++ marilyn/survey/urls.py
@@ -7,7 +7,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('survey', views.index, name='index'),
+    path('', views.index, name='index'),
     path('392e5faa99df48edbda687cc7571a7b8.txt', views.wechat_verification, name='wechat_verification'),
     # path('report', views.report, name='report'),
 ]
--- pilot.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created by panos on 2021/7/11
-# IDE: PyCharm
-
-
-if __name__ == '__main__':
-    pass
